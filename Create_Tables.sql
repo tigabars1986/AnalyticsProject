5c9288481e0ffc035886c4295ed059d48cf209ab
Tigran Barseghyan <t.bars1986@gmail.com>
2026-05-23 11:41:01 +0400

    Add Products Table

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -1,6 +1,13 @@
 CREATE TABLE EmployeeSales (
 SaleID INT PRIMARY KEY IDENTITY(1,1),
 EmployeeName VARCHAR(100),
+Department VARCHAR(50),
 SaleAmount DECIMAL(10,2),
-TransactionDate DATE
+TransactionDate 
+
+CREATE TABLE [Products](
+[Product] [int] PRIMARY KEY IDENTITY (1,1) NOt NULL,
+[ProductName] [nvarchar] (40) NOT NULL,
+[UnitPrice] [money] NULL,
+[Discontinued] [bit] NOT NULL
 );
